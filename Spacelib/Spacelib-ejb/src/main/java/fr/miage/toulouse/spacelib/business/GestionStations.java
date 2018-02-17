b777b1cfde8e768aec9a79d929063b0df862bff9
mahdi <mahdi@mahdi-PC>
2018-02-17 18:10:57 +0100

    tests

---
+++ Spacelib/Spacelib-ejb/src/main/java/fr/miage/toulouse/spacelib/business/GestionStations.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.miage.toulouse.spacelib.business;
+
+import fr.miage.toulouse.spacelib.repositories.StationFacadeLocal;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author mahdi
+ */
+@Stateless
+public class GestionStations implements GestionStationsLocal {
+
+    @EJB
+    private StationFacadeLocal stationFacade;
+    
+    public void creerStation (Long id) {
+        this.stationFacade.creerStation(id);
+    }
+}
