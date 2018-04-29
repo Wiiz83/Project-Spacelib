e3d6d7cbb0c53f7884a5d1925b94ad872e00e55f
Lucas <uzanlah@gmail.com>
2018-04-29 11:07:43 +0200

    Création des services

---
+++ SpacelibShared/src/main/java/com/miage/spacelib/services/ServicesBorneRemote.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.miage.spacelib.services;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author uzanl
+ */
+@Remote
+public interface ServicesBorneRemote {
+    
+}
