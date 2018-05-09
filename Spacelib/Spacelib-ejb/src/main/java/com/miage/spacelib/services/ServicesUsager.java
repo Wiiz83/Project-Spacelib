ebb3bbe7712572d99744d1d46ef7f2ec5240226c
Lucas <uzanlah@gmail.com>
2018-05-10 00:38:22 +0200

    modif ServicesUsager, lancement de l'appli web usager et suppression ServicesBorne et ServicesReservation

--- Spacelib/Spacelib-ejb/src/main/java/com/miage/spacelib/services/ServicesUsager.java
+++ Spacelib/Spacelib-ejb/src/main/java/com/miage/spacelib/services/ServicesUsager.java
@@ -5,10 +5,19 @@
  */
 package com.miage.spacelib.services;
 
+import com.miage.spacelib.business.GestionStationLocal;
+import com.miage.spacelib.business.GestionVoyageLocal;
 import com.miage.spacelib.entities.Quai;
 import com.miage.spacelib.entities.Usager;
 import com.miage.spacelib.entities.Voyage;
+import com.miage.spacelib.exceptions.ClientInconnuException;
+import com.miage.spacelib.exceptions.ReservationClotureeException;
+import com.miage.spacelib.exceptions.ReservationInconnuException;
+import com.miage.spacelib.exceptions.ReservationPasseeException;
+import com.miage.spacelib.exceptions.StationInconnuException;
+import java.util.ArrayList;
 import java.util.Calendar;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -16,33 +25,82 @@ import javax.ejb.Stateless;
  * @author uzanl
  */
 @Stateless
-public class ServicesUsager implements ServicesUsagerLocal {
+public class ServicesUsager implements ServicesUsagerRemote {
+    
+    @EJB
+    private GestionStationLocal gestionStation;
+    
+    @EJB
+    private GestionVoyageLocal gestionVoyage;
+    
 
     @Override
     public boolean login(String nom, String pass) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return false;
     }
-
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
 
     @Override
     public void creeCompte(String nom, String prenom, String login, String motdepasse) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
-    public Voyage reserver(int nbPassagers, Quai quaiDepart, Quai quaiArrivee, Calendar dateDepart) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public long reserver(int nbPassagers, long idQuaiDepart, long idQuaiArrivee, Calendar dateDepart) {
+        return 0;
     }
 
     @Override
-    public Voyage voyageEnCours(Usager usager) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public long voyageEnCours(long idUsager) {
+        return 0;
     }
 
     @Override
-    public void finaliserVoyage(Voyage voyage) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void finaliserVoyage(long idVoyage) {
     } 
+
+    /**
+     * Ce service retourne la liste des stations existantes (incluant leur localisation)
+     * CF 3.1
+     * 
+     * @return la liste des stations existantes
+     */
+    @Override
+    public ArrayList obtenirStations() {
+        return this.gestionStation.obtenir();
+    }
+
+    
+    /**
+     * Ce service permet à un client de réserver un voyage
+     * CF 3.2
+     * 
+     * @param idClient
+     * @param idStationDepart
+     * @param idStationArrivee
+     * @param NbPassagers
+     * @throws com.miage.spacelib.exceptions.ClientInconnuException
+     * @throws com.miage.spacelib.exceptions.StationInconnuException
+     */
+    @Override
+    public void reserverVoyage(Long idClient, Long idStationDepart, Long idStationArrivee, int NbPassagers) throws ClientInconnuException, StationInconnuException {
+       this.gestionVoyage.reserver();
+    }
+
+    
+    /**
+     * Ce service permet à un client d'annuler un voyage
+     * CF 3.3
+     * 
+     * @param idClient l'identifiant du client 
+     * @param idReservation l'identifiant de la réservation à annuler
+     * 
+     * @throws com.miage.spacelib.exceptions.ClientInconnuException si l'id du client est inconnu
+     * @throws com.miage.spacelib.exceptions.ReservationInconnuException si l'id de la réservation est inconnu
+     * @throws com.miage.spacelib.exceptions.ReservationPasseeException si la date de départ est passé 
+     * @throws com.miage.spacelib.exceptions.ReservationClotureeException si la réservation est clôturée
+     */
+    @Override
+    public void annulerVoyage(Long idClient, Long idReservation) throws ClientInconnuException, ReservationInconnuException, ReservationPasseeException, ReservationClotureeException {
+        this.gestionVoyage.annuler(idClient, idReservation);
+    }
 }
--- SpacelibShared/src/main/java/com/miage/spacelib/services/ServicesUsagerRemote.java
+++ SpacelibShared/src/main/java/com/miage/spacelib/services/ServicesUsagerRemote.java
@@ -1,18 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.miage.spacelib.services;
 
-import java.util.List;
+import com.miage.spacelib.exceptions.ClientInconnuException;
+import com.miage.spacelib.exceptions.ReservationClotureeException;
+import com.miage.spacelib.exceptions.ReservationInconnuException;
+import com.miage.spacelib.exceptions.ReservationPasseeException;
+import com.miage.spacelib.exceptions.StationInconnuException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import javax.ejb.Remote;
 
-/**
- *
- * @author uzanl
- */
+
 @Remote
 public interface ServicesUsagerRemote {
-    public boolean login (String nom, String pass) ;    
+    
+    public boolean login (String nom, String pass);    
+    
+    public void creeCompte(String nom, String prenom, String login, String motdepasse);
+    
+    public long reserver(int nbPassagers, long idQuaiDepart, long idQuaiArrivee, Calendar dateDepart);
+    
+    public long voyageEnCours(long idUsager);
+    
+    public void finaliserVoyage(long idVoyage);
+
+    public ArrayList obtenirStations();   
+
+    public void reserverVoyage(Long idClient, Long idStationDepart, Long idStationArrivee, int NbPassagers) throws ClientInconnuException, StationInconnuException;
+
+    public void annulerVoyage(Long idClient, Long idReservation) throws ClientInconnuException, ReservationInconnuException, ReservationPasseeException, ReservationClotureeException;
+
 }
