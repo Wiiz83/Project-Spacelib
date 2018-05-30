32b23f47ec26b2373cb105339aaea7374d963c9f
Lucas <uzanlah@gmail.com>
2018-05-30 14:14:32 +0200

    avancement implémentation equilibrage

--- Spacelib/Spacelib-ejb/src/main/java/com/miage/spacelib/business/Equilibrage.java
+++ Spacelib/Spacelib-ejb/src/main/java/com/miage/spacelib/business/Equilibrage.java
@@ -35,20 +35,31 @@ public class Equilibrage {
         return equilibrer_stations_plus_90p(stations, stations_occ_90p, stations_occ_10p, trajets_10p);
     }
 
-    private List<Entry<Station, Station>> equilibrer_stations_moins_10p(List<Station> stations, List<Station> stations_occ_10p, List<Station> retraits_interdits, ArrayList<Entry<Station, Station>> transferts) {
+    private List<Entry<Station, Station>> equilibrer_stations_moins_10p(List<Station> stations, List<Station> stations_occ_10p, List<Station> stations_occ_90p, ArrayList<Entry<Station, Station>> transferts) {
         List<Station> stations_10p_equilibrees = new ArrayList<>();
+        List<Station> retraits_interdits=stations_occ_90p;
         while (stations_occ_10p.size() >= stations_10p_equilibrees.size()) {
             Station s_inf = stationLaMoinsOccupee(stations_occ_10p, stations_10p_equilibrees);
-            if (ratioApresTransfert(s_inf) > 0.9) {
+            if (ratioApresAjout(s_inf) > 0.9) {
                 stations_10p_equilibrees.add(s_inf);
                 continue;
             }
             while (!stations_10p_equilibrees.contains(s_inf)) {
-                Station s_sup = stationPlusOccupee(stations,retraits_interdits);
+                Station s_sup = stationPlusOccupee(stations,retraits_interdits,stations_occ_90p );
+                if  (s_sup == null) {
+                    return transferts;
+                }
+                if ( ratioApresRetrait(s_sup)<0.10) {
+                    retraits_interdits.add(s_sup);
+                }
+                else {
+                    transferts.add(transfert(s_sup,s_inf));
+                    if (ratioDispo(s_inf)>=0.20){
+                        stations_10p_equilibrees.add(s_inf);
+                    }
+                }
             }
-
         }
-
         return transferts;
     }
 
@@ -78,11 +89,19 @@ public class Equilibrage {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private double ratioApresTransfert(Station s_inf) {
+    private double ratioApresAjout(Station s_inf) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private Station stationPlusOccupee(List<Station> stations, List<Station> retraits_interdits) {
+    private Station stationPlusOccupee(List<Station> stations, List<Station> retraits_interdits, List<Station> stations_occ_90p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private double ratioApresRetrait(Station s_sup) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private double ratioDispo(Station s_inf) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
